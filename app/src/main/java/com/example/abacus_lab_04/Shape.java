5872e731edfc9bfd4faa623ccaa551e77ab00113
Dyna Dagman <dagman@ualberta.ca>
2026-01-26 17:10:40 -0700

    Added shape java class with fields, constructor, and setters/getters

---
+++ app/src/main/java/com/example/abacus_lab_04/Shape.java
@@ -0,0 +1,31 @@
+package com.example.abacus_lab_04;
+
+public class Shape {
+    private int x;
+    private int y;
+
+    public Shape() {
+        this.x = 0;
+        this.y = 0;
+    }
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
