c9230f6667905931f7d970540e4d13891df5f68a
Dyna Dagman <dagman@ualberta.ca>
2026-01-29 18:41:46 -0700

    update to hexagon.java
    ???

--- app/src/main/java/com/example/abacus_lab_04/Hexagon.java
+++ app/src/main/java/com/example/abacus_lab_04/Hexagon.java
@@ -1,4 +1,8 @@
 package com.example.abacus_lab_04;
 
-public class Hexagon extends Shape {
+public class hexagon extends Shape{
+    public hexagon(int x, int y) {
+            super(x, y);
+    }
 }
+
